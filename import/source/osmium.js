a75b70ce9b83e0ac4034e1559baa7b6c02646b2d
missinglink <insomnia@rcpt.at>
2019-07-17 17:42:28 +0200

    feat(osm): improved tag mapping of names

--- import/source/osmium.js
+++ import/source/osmium.js
@@ -24,25 +24,25 @@ const mapping = {
     const properties = _.get(record, 'properties', {})
     for (let attr in properties) {
       // https://wiki.openstreetmap.org/wiki/Multilingual_names
-      if (!attr.startsWith('name:')) { continue }
-      let match = attr.match(/^name:([a-z]{2,3})$/)
+      if (!attr.startsWith('name:') && !attr.startsWith('alt_name:')) { continue }
+      let match = attr.match(/^(name|alt_name):([a-z]{2,3})$/)
       if (match) {
-        let lang = match[1]
+        let lang = match[2]
 
         // convert iso codes 639-1 to 639-3
         if (lang.length === 2) {
-          let locales = new locale.Locales(match[1])
+          let locales = new locale.Locales(match[2])
           let best = locales.best(allLocales)
           lang = language[best.language].iso6393
         }
 
         // skip if both iso codes 639-1 and 639-3 are present
-        if (lang !== match[1] && properties['name:' + lang]) { continue }
+        if (lang !== match[2] && properties['name:' + lang]) { continue }
 
         // index name
         names.push({
           lang: lang,
-          tag: 'preferred',
+          tag: (match[1] === 'name') ? 'preferred' : 'variant',
           abbr: 0,
           name: properties[attr]
         })
@@ -51,11 +51,37 @@ const mapping = {
     return names
   },
   'property': (record) => {
-    return _.pickBy(record.properties, (val, key) => {
+    let props = {}
+
+    // generic properties
+    props.name = _.get(record, 'properties.name', '').toUpperCase()
+
+    // country codes
+    // https://wiki.openstreetmap.org/wiki/Country_code
+
+    // ISO 3166-1
+    props.alpha2 = _.get(record, 'properties.ISO3166-1:alpha2', '').toUpperCase()
+    props.alpha3 = _.get(record, 'properties.ISO3166-1:alpha3', '').toUpperCase()
+
+    // ISO 3166-2
+    let subdivision = _.get(record, 'properties.ISO3166-2', '').toUpperCase()
+    let parts = subdivision.split('-')
+    if (parts.length === 2 && parts[0].length === 2) {
+      props.alpha2 = parts[0]
+      props.abbr = parts[1]
+    }
+
+    // osm-specific properties
+    _.assign(props, _.pickBy(record.properties, (val, key) => {
       if (key.startsWith('@')) { return false }
       if (key.startsWith('name:')) { return false }
+      if (key.startsWith('alt_name:')) { return false }
+      if (key.startsWith('ISO3166-1:alpha')) { return false }
+      if (key.startsWith('ISO3166-2')) { return false }
       return true
-    })
+    }))
+
+    return props
   }
 }
 
