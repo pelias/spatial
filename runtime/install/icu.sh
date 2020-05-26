96be4467eabd06fd7b69a817461a4efe8c9b0b51
missinglink <insomnia@rcpt.at>
2020-05-26 14:38:27 +0200

    fix(runtime): simplify ICU rpath

--- runtime/install/icu.sh
+++ runtime/install/icu.sh
@@ -18,12 +18,10 @@ curl -L 'http://github.com/unicode-org/icu/releases/download/release-67-1/icu4c-
 # working directory
 cd icu/source
 
-# set 'rpath'
-export LDFLAGS="-Wl,-rpath,${RUNTIME}/lib"
-
 # configure build
 ./configure \
   --prefix="${RUNTIME}" \
+  --enable-rpath \
   --disable-debug \
   --disable-samples \
   --disable-tests \
@@ -32,18 +30,6 @@ export LDFLAGS="-Wl,-rpath,${RUNTIME}/lib"
 # compile and install in runtime directory
 make -j8
 make install
-
-## tell DYLIB files on Mac to use rpath
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  install_name_tool -id '@rpath/libicui18n.67.dylib' "${RUNTIME}/lib/libicui18n.67.dylib"
-  install_name_tool -change 'libicuuc.67.dylib' '@rpath/libicuuc.67.dylib' "${RUNTIME}/lib/libicui18n.67.dylib"
-  install_name_tool -change 'libicudata.67.dylib' '@rpath/libicudata.67.dylib' "${RUNTIME}/lib/libicui18n.67.dylib"
-
-  install_name_tool -id '@rpath/libicuuc.67.dylib' "${RUNTIME}/lib/libicuuc.67.dylib"
-  install_name_tool -change 'libicudata.67.dylib' '@rpath/libicudata.67.dylib' "${RUNTIME}/lib/libicuuc.67.dylib"
-
-  install_name_tool -id '@rpath/libicudata.67.dylib' "${RUNTIME}/lib/libicudata.67.dylib"
-fi
 
 # clean up
 rm -rf /tmp/icu
