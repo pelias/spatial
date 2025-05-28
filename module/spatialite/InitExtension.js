afbcf34db483f02165d5d18b334ba47b45e8d7d9
Peter Johnson <insomnia@rcpt.at>
2025-05-28 13:59:37 +0200

    spatialite: homebrew support

--- module/spatialite/InitExtension.js
+++ module/spatialite/InitExtension.js
@@ -1,10 +1,30 @@
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
 const Sqlite = require('../../sqlite/Sqlite')
+const modulePaths = ['mod_spatialite']
+
+// support for homebrew: `brew install libspatialite`
+const homebrewDir = '/opt/homebrew/Cellar/libspatialite'
+if (os.platform() === 'darwin' && fs.existsSync(homebrewDir)) {
+  modulePaths.push(
+    ...fs.readdirSync(homebrewDir, { recursive: true })
+      .filter(f => f.endsWith('mod_spatialite.dylib'))
+      .map(f => path.join(homebrewDir, f))
+      .sort().reverse()
+  )
+}
 
 // load the spatialite extension
 class InitExtension extends Sqlite {
   load (db) {
-    db.loadExtension('mod_spatialite')
-    return true
+    for (const path of modulePaths) {
+      try {
+        db.loadExtension(path)
+        return true
+      } catch (e) {}
+    }
+    throw new Error('failed to load spatialite extension')
   }
   create (db) {
     if (!this.load(db)) {
