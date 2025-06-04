7bf465bdf1c2f2b0b6469d6e004aa9a8058a49b6
Peter Johnson <insomnia@rcpt.at>
2025-06-04 11:33:38 +0200

    pip: legacy compatible pip

--- server/routes/pip_verbose.js
+++ server/routes/pip_verbose.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const util = require('./util')
 const iso6393 = require('iso-639-3')
 const language = {}
@@ -11,31 +12,31 @@ iso6393.filter(i => !!i.iso6391 && !!i.iso6393).forEach(i => { language[i.iso639
  * set @wofonly = 1 to only show results from whosonfirst
  **/
 module.exports = function (req, res) {
-  var service = req.app.locals.service
+  const service = req.app.locals.service
 
   const lang = util.flatten(req.query.lang)
   const sources = util.flatten(req.query.wofonly) ? ['wof'] : util.toSources(req.query.sources)
 
   // inputs
-  let query = {
+  const query = {
     lon: parseFloat(util.flatten(req.query.lon)),
     lat: parseFloat(util.flatten(req.query.lat)),
     limit: 1000,
     aliaslimit: parseInt(util.flatten(req.query.aliaslimit), 10) || 0,
     sources: sources.length > 0 ? RECORD_SEPARATOR + sources.join(RECORD_SEPARATOR) + RECORD_SEPARATOR : '',
-    lang: language[lang] || lang || 'und'
+    lang: language[lang] || lang || 'und',
+    hierarchy: req.query.hierarchy ? 1 : 0
   }
 
   // perform query
   // console.time('took')
-  let rows = service.module.pip.statement.verbose.all(query)
+  const rows = service.module.pip.statement.verbose.all(query)
   // console.timeEnd('took')
 
   // rewrite response to emulate 'wof-admin-lookup' format
   let resp = {}
   rows.forEach(row => {
-    let centroid = row.centroid.split(',').map(util.floatPrecision7)
-    let name = row.name_localized || row.name || undefined
+    const name = row.name_localized || row.name || undefined
 
     let nameAlias = []
     if (query.aliaslimit > 0) { nameAlias = (row.names || '').split(String.fromCharCode(30)) }
@@ -45,19 +46,28 @@ module.exports = function (req, res) {
     if (query.aliaslimit > 0) { abbrAlias = (row.abbrs || '').split(String.fromCharCode(30)) }
     abbrAlias = (abbrAlias.length > 1) ? abbrAlias.filter(n => n !== name).slice(0, query.aliaslimit) : undefined
 
+    let hierarchy = {}
+    if (query.hierarchy) {
+      hierarchy = _.mapValues(
+        JSON.parse(row.hierarchy || '{}'),
+        (parent) => {
+          _.assign(parent, boundsAndCentroid(parent))
+          delete parent.bounds
+          return parent
+        }
+      )
+    }
+
     if (!Array.isArray(resp[row.type])) { resp[row.type] = [] }
     resp[row.type].push({
       id: row.id,
       source: row.source,
-      name: name,
+      name,
       name_alias: nameAlias,
       abbr: row.abbr || undefined,
       abbr_alias: abbrAlias,
-      centroid: {
-        lat: centroid[1],
-        lon: centroid[0]
-      },
-      bounding_box: row.bounds.split(',').map(util.floatPrecision7).join(',')
+      ...boundsAndCentroid(row),
+      hierarchy
     })
   })
 
@@ -68,4 +78,16 @@ module.exports = function (req, res) {
 
   // send json
   res.status(200).json(resp)
+}
+
+function boundsAndCentroid (row) {
+  const centroidString = (row.centroid ? row.centroid : '0,0')
+  const c = centroidString.split(',').map(util.floatPrecision7)
+
+  return {
+    centroid: { lat: c[1], lon: c[0] },
+    bounding_box: (
+      row.bounds ? row.bounds.split(',') : [c[0], c[1], c[0], c[1]]
+    ).map(util.floatPrecision7).join(',')
+  }
 }
