6d806abf8fadfb5c8c90417efeafff20c329306a
Peter Johnson <insomnia@rcpt.at>
2025-06-18 14:24:55 +0200

    pip(pelias): untrusted layer support, improve tests

--- server/routes/pip_pelias.js
+++ server/routes/pip_pelias.js
@@ -1,6 +1,12 @@
 const _ = require('lodash')
 const verbose = require('./pip_verbose')
-const layerCompatibility = new Set([
+const untrustedLayers = new Set(['neighbourhood'])
+
+/**
+ * @todo: only search layers provided in query
+ */
+
+const defaultLayers = new Set([
   'neighbourhood',
   'borough',
   'locality',
@@ -33,49 +39,53 @@ module.exports = function (req, res) {
 }
 
 // rewite the verbose view to match the expected format
-// function remap (resp) {
-//   for (let placetype in resp) {
-//     if (layerCompatibility.has(placetype)) {
-//       resp[placetype] = resp[placetype].map(row => {
-//         return {
-//           id: parseInt(row.id, 10),
-//           name: row.name,
-//           abbr: row.abbr,
-//           centroid: row.centroid,
-//           bounding_box: row.bounding_box
-//         }
-//       })
-//     } else {
-//       delete resp[placetype]
-//     }
-//   }
-//   return resp
-// }
-
-// rewite the verbose view to match the expected format
 // using the 'lowest' matching placetype as the base and adopting
 // the hierarchy from that record for the parents.
 function remapFromHierarchy (resp) {
-  for (const layer of layerCompatibility) {
-    const lowest = _.get(resp, layer) // find 'lowest' layer matches (array)
-    if (lowest) {
-      const r = {
-        [layer]: lowest.map(place => {
-          place.id = parseInt(place.id, 10) // for compatibility
-          return _.omit(place, ['hierarchy', 'source'])
-        })
-      }
-      lowest.forEach(place => {
-        for (const placetype in place.hierarchy) {
-          if (!r[placetype]) { r[placetype] = [] }
-          const parent = place.hierarchy[placetype]
-          parent.id = parseInt(parent.id, 10) // for compatibility
-          if (r[placetype].some(v => v.id === parent.id)) { continue } // dedupe
-          r[placetype].push(parent)
-        }
-      })
-      return r
-    }
+  const mapped = {}
+  let chosen = [] // the chosen 'lowest layer' to use for the hierarchy
+
+  // iterate through compatible layers
+  for (const layer of defaultLayers) {
+    if (!_.has(resp, layer)) { continue }
+    chosen = _.get(resp, layer)
+
+    // map properties from the lowest matching layer
+    mapped[layer] = chosen.map(normalize)
+
+    // don't trust the hierarchy from some 'untrusted layers'
+    // https://github.com/pelias/wof-admin-lookup/pull/246
+    if (!untrustedLayers.has(layer)) { break }
   }
-  return {}
+
+  // iterate through chosen hits (there may be multiple hits for this one placetype)
+  // and adopt the hierarchy from each.
+  chosen.forEach(place => {
+    for (const [placetype, parent] of Object.entries(place.hierarchy)) {
+      const norm = normalize(parent)
+      if (!Array.isArray(mapped[placetype])) { mapped[placetype] = [] }
+      mapped[placetype].push(norm)
+    }
+  })
+
+  // dedupe and clean results
+  for (const placetype of Object.keys(mapped)) {
+    // remove any placetypes not in the compatibility list
+    // note: we may elect to remove this filter in the future
+    if (!defaultLayers.has(placetype)) {
+      delete mapped[placetype]
+      continue
+    }
+
+    mapped[placetype] = _.uniqBy(mapped[placetype], 'id')
+  }
+
+  return mapped
+}
+
+function normalize (place) {
+  const res = _.pick(place, ['id', 'name', 'abbr', 'centroid', 'bounding_box'])
+  res.id = parseInt(res.id, 10) // for compatibility (wof IDs are always numeric)
+  if (!res.abbr) { delete res.abbr }
+  return res
 }
