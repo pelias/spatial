062c9a4440bc9968be152c6c7f6098796ff08b9f
Peter Johnson <insomnia@rcpt.at>
2025-08-28 15:23:33 +0200

    refactor(pip): refactor "remap" to avoid use of Function.prototype.bind()

--- server/routes/pip_verbose.js
+++ server/routes/pip_verbose.js
@@ -73,8 +73,8 @@ module.exports = function (req, res) {
   })
 
   // allow other views to utilize this view
-  if (typeof this.remap === 'function') {
-    resp = this.remap(resp, req, res)
+  if (typeof req.remap === 'function') {
+    resp = req.remap(resp, req, res)
   }
 
   // send json
