d36434e3b29fb4b057c09da70f76e6de813d2354
Peter Johnson <738069+missinglink@users.noreply.github.com>
2025-11-12 15:08:40 +0100

    feat(spatialite): enable TinyPoint

--- module/spatialite/InitSpatialOptions.js
+++ module/spatialite/InitSpatialOptions.js
@@ -10,16 +10,15 @@ class InitSpatialOptions extends Sqlite {
       this.error('SetDecimalPrecision', e)
     }
 
-    // disabled: it's convenient to be able to still use GeometryType() on 4.3.0a
     // Reduce the number of bytes required to represent a POINT geometry
     // note: this is a libspatialite-5.0.0 feature, databases generated with this setting
     // may not be read correctly by older versions of spatialite.
     // see: https://www.gaia-gis.it/fossil/libspatialite/wiki?name=BLOB-TinyPoint
-    // try {
-    //   db.prepare(`SELECT EnableTinyPoint()`).get()
-    // } catch (e) {
-    //   this.error('EnableTinyPoint', e)
-    // }
+    try {
+      db.prepare(`SELECT EnableTinyPoint()`).get()
+    } catch (e) {
+      this.error('EnableTinyPoint', e)
+    }
   }
 }
 
